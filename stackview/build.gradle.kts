84d4d930e872cb6ef4d8288bf5ffd95e55b2eb42
Joseph Milan K.A <josmilan1103@gmail.com>
2026-03-17 11:08:52 +0530

    Add: GitHub Packages publishing and Gradle wrapper

--- stackview/build.gradle.kts
+++ stackview/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -23,4 +24,42 @@ android {
 
 dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "io.igrant"
+                artifactId = "stackview"
+                version = findProperty("VERSION_NAME") as String? ?: "1.0.0"
+
+                pom {
+                    name.set("StackView")
+                    description.set("A custom RecyclerView.LayoutManager for wallet-style stacked cards")
+                    url.set("https://github.com/L3-iGrant/stack-view")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/L3-iGrant/stack-view")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR") ?: findProperty("gpr.user") as String? ?: ""
+                    password = System.getenv("GITHUB_TOKEN") ?: findProperty("gpr.token") as String? ?: ""
+                }
+            }
+        }
+    }
 }
